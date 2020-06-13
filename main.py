bda2cc5d1490122d236583eef08ad0eb467bddd5
Ivan Aguas <aguas.xavier2013a@gmail.com>
2020-06-13 11:26:58 -0500

    Add subplot of PCA and TNSE

--- main.py
+++ main.py
@@ -87,3 +87,40 @@ time_start = time.time()
 tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
 tsne_results = tsne.fit_transform(data_subset)
 print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
+
+df_subset['tsne-2d-one'] = tsne_results[:,0]
+df_subset['tsne-2d-two'] = tsne_results[:,1]
+plt.figure(figsize=(16,10))
+sns.scatterplot(
+    x="tsne-2d-one", y="tsne-2d-two",
+    hue="y",
+    palette=sns.color_palette("hls", 10),
+    data=df_subset,
+    legend="full",
+    alpha=0.3
+)
+
+
+# Add graphs
+
+plt.figure(figsize=(16,7))
+ax1 = plt.subplot(1, 2, 1)
+sns.scatterplot(
+    x="pca-one", y="pca-two",
+    hue="y",
+    palette=sns.color_palette("hls", 10),
+    data=df_subset,
+    legend="full",
+    alpha=0.3,
+    ax=ax1
+)
+ax2 = plt.subplot(1, 2, 2)
+sns.scatterplot(
+    x="tsne-2d-one", y="tsne-2d-two",
+    hue="y",
+    palette=sns.color_palette("hls", 10),
+    data=df_subset,
+    legend="full",
+    alpha=0.3,
+    ax=ax2
+)
